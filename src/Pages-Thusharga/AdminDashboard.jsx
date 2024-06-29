83602a0045d425b5952e9d4c30a39aa5a71e15dc
Chamara17-cc <149788803+Chamara17-cc@users.noreply.github.com>
2024-06-29 12:25:47 +0530

    userid  error

--- src/Pages-Thusharga/AdminDashboard.jsx
+++ src/Pages-Thusharga/AdminDashboard.jsx
@@ -3,14 +3,10 @@ import Sidebar from '../Compornents/Sidebar'
 import Topbar from '../Compornents/Topbar'
 import '../Pages/PageStructure.css'
 import AdminDashboardComponent from '../Components-Thusharaga/AdminDashboardComponent'
-import { useLocation } from 'react-router-dom'
 
 
 function AdminDashboard() {
 
-
-  const location = useLocation();
-  const x = location.state.userId;
 
 
   return (
