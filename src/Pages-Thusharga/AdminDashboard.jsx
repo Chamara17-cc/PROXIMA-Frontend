bcaf4d88325de0114147e4411eb6fbeaac56d92c
Chamara17-cc <149788803+Chamara17-cc@users.noreply.github.com>
2024-06-29 20:22:14 +0530

    t

--- src/Pages-Thusharga/AdminDashboard.jsx
+++ src/Pages-Thusharga/AdminDashboard.jsx
@@ -9,9 +9,7 @@ import { useLocation } from 'react-router-dom'
 function AdminDashboard() {
 
 
-  const location = useLocation();
-  const x = location.state.userId;
-
+ 
 
   return (
     <div className="FullPage">
